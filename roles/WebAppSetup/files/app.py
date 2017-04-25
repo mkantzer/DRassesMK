40a33f8db2b4535205f755e57e3f91689a68099b
Mike Kantzer <mbkantzer>
2017-04-25 10:29:42 -0400

    updated primary webapp to parse for inputs using reqparse

--- roles/WebAppSetup/files/app.py
+++ roles/WebAppSetup/files/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from pymongo import MongoClient
 import hashlib, datetime, pprint
-from dateutil import parser
+from dateutil import parser as dateparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,10 +15,19 @@ db = client.logging_database
 logs = db.logs
 
 
+#set up request parser
+parser = reqparse.RequestParser()
+parser.add_argument('uid', required=True, help="uid required for this request")
+parser.add_argument('date', required=True, help="uid required for this request")
+
 
 class Posting(Resource):
 	def post(self):
-		payload = request.get_json(force=True)
+		#grab name and checksum from json
+		parser.add_argument('name', location='json', required=True)
+		parser.add_argument('md5checksum', location='json', required=True)
+		payload = parser.parse_args()
+#		pprint.pprint(payload)
 		# Generate checksum
 		m = hashlib.md5()
 		StringtoChecksum = '''{"date": "%(date)s", "uid": "%(uid)s", "name": "%(name)s"}''' %{'date': payload['date'], 'uid':payload['uid'], 'name': payload['name']}
@@ -26,21 +35,19 @@ class Posting(Resource):
 		# Verify checksums equal
 		if payload['md5checksum'].lower() == m.hexdigest().lower():
 			#convert given date format to datetime.datetime
-			newdate = parser.parse(payload['date'])
+			newdate = dateparse.parse(payload['date'])
 			#create dictonary/doc
 			data = {"uid": payload['uid'],
 					"name": payload['name'],
 					"date": newdate,
 					"md5checksum": payload['md5checksum']}
 #			pprint.pprint(data)		
+			# insert doc into logs collection
 			post_id = logs.insert_one(data).inserted_id
 			return "Successfully inserted with post_id %s"%post_id
 		else:
 			#if checksums arent equal
 			return "Error 400, bad checksum"
-
-#class Getting(Resource):
-#	def get(self, ):
 
 
 #this may need to be under a different class/resource
