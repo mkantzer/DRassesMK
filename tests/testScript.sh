062febe4063d41102284559f9a7a36dbfda67161
Mike Kantzer <mbkantzer>
2017-04-25 13:18:29 -0400

    no message

--- tests/testScript.sh
+++ tests/testScript.sh
@@ -2,8 +2,15 @@
 
 # write a bunch of files to server
 curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-12T14:36:00.451765", "uid": "1", "name": "John Doe", "md5checksum": "E8C83E232B64CE94FDD0E4539AD0D44F"}'
-curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-13T14:36:00.451765", "uid": "2", "name": "Mike Kantzer", "md5checksum": "D0E571C33CD39366E186260649BCBB14"}'
-curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-08-31T14:36:00.451765", "uid": "4", "name": "Jane Franko", "md5checksum": "B150909CE797CD36C3B15322A4C3776F"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-12T14:40:00.451765", "uid": "1", "name": "John Doe", "md5checksum": "92D5B45A67FFAEB8358FF155DC6CDAF6"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-12T23:59:00.451765", "uid": "1", "name": "John Doe", "md5checksum": "F439EBE7B0A31FB985996E69F1818AD6"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-13T14:36:00.451765", "uid": "1", "name": "John Doe", "md5checksum": "EFBA495B9ECB5F8607434A933E53BFB9"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-14T14:36:00.451765", "uid": "1", "name": "John Doe", "md5checksum": "8F5C5BD7393BEF16BBD2A89ED7913F21"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-12T14:36:00.451765", "uid": "2", "name": "Mike Kan", "md5checksum": "43248CBC009EA7A9B4F36D040BDB9515"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-12T14:40:00.451765", "uid": "2", "name": "Mike Kan", "md5checksum": "9D2DBB1555A7365428299C58277C628C"}'
+curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d '{"date": "2015-05-13T14:36:00.451765", "uid": "2", "name": "Mike Kan", "md5checksum": "EFA6EFAACB0EA1B88537AC1A6CF5D07E"}'
+#curl 192.168.33.10:8080/post -X POST -H "Content-Type: application/json" -d ', "md5checksum": ""}'
+
 
 
 # execute gets
