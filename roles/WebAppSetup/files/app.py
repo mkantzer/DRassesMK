15da40afe6354355066bbdd6a0acfc634f155b7b
Mike Kantzer <mbkantzer>
2017-04-24 23:01:35 -0400

    updated app for a while

--- roles/WebAppSetup/files/app.py
+++ roles/WebAppSetup/files/app.py
@@ -1,28 +1,38 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
 from flask_pymongo import PyMongo
+import hashlib
 
 app = Flask(__name__)
 api = Api(app)
 mongo = PyMongo(app)
 
+class Posting(Resource):
+	def post(self):
+		payload = request.get_json(force=True)
+		# Generate checksum
+		m = hashlib.md5()
+		StringtoChecksum = '''{"date": "%(date)s", "uid": "%(uid)s", "name": "%(name)s"}''' %{'date': payload['date'], 'uid':payload['uid'], 'name': payload['name']}
+		m.update(StringtoChecksum)
+		# Verify checksums equal
+		if payload['md5checksum'].lower() == m.hexdigest().lower():
+			return "this is great"
+		else:
+			return "nope"
 
-#class User(Resource):
-#	def post(self, payload):
-
+#this may need to be under a different class/resource
 #	def get(self, uid, date):
 
 
-
 # First endpoint, for receiving POSTs
-# api.add_resource()
+api.add_resource(Posting, '/post')
 
 
 
 # for checking connection. remove in final submission
-@app.route("/checkConn")
-def hello():
-	return "Hello World!"
+#@app.route("/checkConn")
+#def hello():
+#	return "Hello World! noDB.py \n"
 
 
 if __name__ == '__main__':
